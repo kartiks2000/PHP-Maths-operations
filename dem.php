0dba6b384abac44057160ab5b7ac8441575472ff
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-08-02 11:36:00 +0530

    Add files via upload

---
+++ dem.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Maths in PHP</h1>
+    <?php 
+        // Math Operators
+        echo 5+2;
+        echo "<br>";
+        echo 3*5;
+        echo "<br>";
+        echo 100-20;
+        echo "<br>";
+        echo 20/3;
+        echo "<br>";
+        echo 20%3;
+        echo "<br>";
+        
+        echo "<br>";
+        $num1 = 27;
+        $num2 = 6;
+        $num3 = $num1 + $num2;
+        echo $num3;
+
+        echo "<br>";
+        echo "<br>";
+
+        echo $num1 + $num2;
+    ?>
+</body>
+</html>
